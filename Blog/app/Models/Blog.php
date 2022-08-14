367d1be832b947cbc7174371bae56a4e0a80978f
TanhaR31 <tanurezza99@gmail.com>
2022-08-14 20:54:38 +0600

    user_id added as blogger_id

--- Blog/app/Models/Blog.php
+++ Blog/app/Models/Blog.php
@@ -4,12 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Blogger;
 
 class Blog extends Model
 {
     public $timestamps = false;
     protected $fillable = [
         'blogger_id', 'title', 'slug', 'description', 'image'
-    ];
+    ]; //user_id is named as blogger_id
+    public function blogBelongsToBloggers()
+    {
+        return $this->belongsTo(Blogger::class, 'blogger_id', 'id');  //user_id is named as blogger_id
+    }
     use HasFactory;
 }
--- Blog/app/Models/Blogger.php
+++ Blog/app/Models/Blogger.php
@@ -4,13 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Blog;
 
 class Blogger extends Model
 {
-    public $timestamps=false;
+    public $timestamps = false;
     protected $fillable = [
         'b_name', 'b_phone', 'b_email', 'b_password', 'b_address', 'b_image'
     ];
-
+    public function bloggerHasManyBlogs()
+    {
+        return $this->hasMany(Blog::class, 'blogger_id', 'id'); //user_id is named as blogger_id
+    }
     use HasFactory;
 }
